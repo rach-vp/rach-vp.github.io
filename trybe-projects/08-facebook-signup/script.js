ce5f090768848db3032b3a9c9304c75b0bd205fc
rach-vp <picanco.raquel@gmail.com>
2021-01-21 19:40:46 -0400

    Implement population of birthday fields by script

--- trybe-projects/08-facebook-signup/script.js
+++ trybe-projects/08-facebook-signup/script.js
@@ -1,3 +1,40 @@
+// População dos campos de data de nascimento
+const birthDaySelect = document.querySelector('#birth-day');
+const birthMonthSelect = document.querySelector('#birth-month');
+const birthYearSelect = document.querySelector('#birth-year');
+
+function createDays() {
+  for (let index = 1; index <= 31; index += 1) {
+    const newDayOption = document.createElement('option');
+    newDayOption.innerText = index.toLocaleString('pt-BR', {minimumIntegerDigits: 2, useGrouping: false});
+    newDayOption.value = index;
+    birthDaySelect.appendChild(newDayOption);
+  }
+}
+
+function createMonths() {
+  const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
+  for (let index = 0; index < months.length; index += 1) {
+    const newMonthOption = document.createElement('option');
+    newMonthOption.innerText = months[index];
+    newMonthOption.value = months[index];
+    birthMonthSelect.appendChild(newMonthOption);
+  }
+}
+
+function createYears() {
+  for (let index = 2021; index >= 1905; index -= 1) {
+    const newYearOption = document.createElement('option');
+    newYearOption.innerText = index;
+    newYearOption.value = index;
+    birthYearSelect.appendChild(newYearOption);
+  }
+}
+
+createDays();
+createMonths();
+createYears();
+
 // Alert com email
 function showAlert() {
   const buttonLogin = document.querySelector('#button-login');
@@ -47,8 +84,6 @@ function isGenderInputChecked() {
 function createHelloUserElement() {
   const userFirstName = document.querySelector('#firstname').value;
   const userLastName = document.querySelector('#lastname').value;
-  // const helloPhrase = document.createElement('p');
-  // helloPhrase.innerText = `Hello, ${userFirstName} ${userLastName}`;
   const helloPhrase = `Olá, ${userFirstName} ${userLastName}`;
   return helloPhrase;
 }
@@ -88,9 +123,6 @@ function showUserData() {
   clearContainer(rightContainer);
   const newFieldsContent = fieldsContent.join(' ');
   rightContainer.innerText = newFieldsContent;
-  // for (let index = 0; index < fieldsContent.length; index += 1) {
-  //   rightContainer.appendChild(fieldsContent[index]);
-  // }
 }
 
 function checkFields() {
